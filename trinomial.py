fe7bc52f3923e0ccd833822a0a59ff798c8c365b
Georgiy Karataev <jorrtone@gmail.com>
2019-03-31 14:15:52 +0200

    Update trinomial.py

--- trinomial.py
+++ trinomial.py
@@ -1,16 +1,3 @@
-#
-# Python program to print the Trinomial triangle
-# (see https://en.wikipedia.org/wiki/Trinomial_triangle)
-#
-# Run with "py trinomial.py [arg]" command, where [arg]
-# is the max height of printed triangle
-#
-# trit_sim(length, total) function can be also used for finding all
-# possible variations of (length) long lists
-# of -1, 0 and 1 elements, that in total give (total) value
-#
-
-
 import sys
 
 
