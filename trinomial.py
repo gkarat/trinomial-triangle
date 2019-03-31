924d2a01dce4449dd6d9d3265eb51bd5f5ade9e2
Georgiy Karataev <jorrtone@gmail.com>
2019-03-31 11:41:27 +0200

    Update trinomial.py

--- trinomial.py
+++ trinomial.py
@@ -1,9 +1,12 @@
 #
-# Python program to print the Trinomial triangle (see https://en.wikipedia.org/wiki/Trinomial_triangle)
+# Python program to print the Trinomial triangle
+# (see https://en.wikipedia.org/wiki/Trinomial_triangle)
 #
-# Run with "py trinomial.py [arg]" command, where [arg] is the max height of printed triangle
-# 
-# trit_sim(length, total) function can be also used for finding all possible variations of (length) long lists 
+# Run with "py trinomial.py [arg]" command, where [arg]
+# is the max height of printed triangle
+#
+# trit_sim(length, total) function can be also used for finding all
+# possible variations of (length) long lists
 # of -1, 0 and 1 elements, that in total give (total) value
 #
 
@@ -14,16 +17,14 @@ import sys
 def temp(res, length, total, initArray):
 
     if length == 0:
-
         if sum(initArray) == total:
             res.append(initArray)
             return
-
         else:
             return
-    
+
     for i in range(-1, 2):
-        temp(res, length-1, total, initArray+[i])
+        temp(res, length - 1, total, initArray + [i])
 
     return res
 
@@ -31,11 +32,9 @@ def temp(res, length, total, initArray):
 def trit_sum(length, total):
 
     if (abs(total) > length):
-
         return []
 
     if (abs(total) == length):
-
         if total < 0:
             return [[-1 for i in range(length)]]
 
@@ -43,25 +42,24 @@ def trit_sum(length, total):
             return [[1 for i in range(length)]]
 
     res = []
-    
+
     for i in range(-1, 2):
-        temp(res, length-1, total, [i])
+        temp(res, length - 1, total, [i])
 
     return res
 
 if __name__ == "__main__":
-    
     MAX_LENGTH = int(sys.argv[1])
 
     for i in range(MAX_LENGTH):
         for j in range(-MAX_LENGTH, MAX_LENGTH + 1):
-            
+
             out = len(trit_sum(i, j))
 
             if out == 0:
-                print("".center(6), end = '')
+                print("".center(6), end='')
                 continue
-            
-            print("{}".format(out).center(6), end = '')
-        
+
+            print("{}".format(out).center(6), end='')
+
         print()
